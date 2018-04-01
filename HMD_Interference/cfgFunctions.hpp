cfd0c4a928ca0e5ed8b726173e77a31b2abccc83
Rodeostar42 <rodeosta42@gmail.com>
2018-04-01 18:00:37 +0900

    ccscscs

---
+++ HMD_Interference/cfgFunctions.hpp
@@ -0,0 +1,8 @@
+class TB_Script
+{
+    class HMD
+    {
+        file = "\HMD_Interference\functions";
+        class HMD_Interference {postInit = 3};        
+    };
+};
---
+++ HMD_Interference/config.cpp
@@ -0,0 +1,17 @@
+
+class CfgPatches 
+{
+	class HMD
+	{
+		units[] = {};
+		requiredVersion = 1.0.4;
+		requiredAddons[] = {"Extended_EventHandlers"};	
+		author[]= {"Rodeostar42"}; 		
+	};
+};
+
+class CfgFunctions
+{
+    #include "CfgFunctions.hpp"
+};
+
